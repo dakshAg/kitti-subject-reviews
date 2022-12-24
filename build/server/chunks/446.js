c8bf0f57eac621f4305a7f9e18556aa38069378b
dakshagrawal <dakshagrawalx@gmail.com>
2022-12-24 22:06:33 +0530

    Fix gitignore

---
+++ build/server/chunks/446.js
@@ -0,0 +1,41 @@
+exports.id = 446;
+exports.ids = [446];
+exports.modules = {
+
+/***/ 9285:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"main": "Home_main__nLjiQ"
+};
+
+
+/***/ }),
+
+/***/ 8699:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "q": () => (/* binding */ firebaseConfig)
+/* harmony export */ });
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// No Keys are leaked here. They can stay public. Refer to https://firebase.google.com/docs/projects/api-keys
+const firebaseConfig = {
+    apiKey: "AIzaSyCJ_JA23pg58RYXdbZDnYOiPL4zOJSZEv8",
+    authDomain: "kitti-subject-reviews.firebaseapp.com",
+    databaseURL: "https://kitti-subject-reviews-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "kitti-subject-reviews",
+    storageBucket: "kitti-subject-reviews.appspot.com",
+    messagingSenderId: "701742452047",
+    appId: "1:701742452047:web:c4f7d61726ac8316ab493a",
+    measurementId: "G-SHYNFEMNLT"
+};
+
+
+/***/ })
+
+};
+;
